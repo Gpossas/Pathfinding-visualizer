df041465e60565973419148e340f75fa4498ea81
Gpossas <gpossas@protonmail.com>
2023-09-18 14:27:08 -0300

    add type notation

--- helpers/board.js
+++ helpers/board.js
@@ -1,3 +1,7 @@
+/**
+ * @param { int } rows
+ * @param { int } columns
+*/
 export function createBoard( rows, columns ){
   for ( let row = 0; row < rows; row++ ){
     for ( let column = 0; column < columns; column++ ){
@@ -13,12 +17,16 @@ export function createBoard( rows, columns ){
  * @param { HTMLElement } board
  * @param { int } rows
  * @param { int } columns
- */
+*/
 export function setBoardDimensions( board, vertexSize, rows, columns ){
   board.style.gridTemplateRows = `${ vertexSize }px `.repeat( rows );
   board.style.gridTemplateColumns = `${ vertexSize }px `.repeat( columns );
 }
 
+/**
+ * @param { String } html
+ * @return { HTMLElement } 
+*/
 function htmlToElement( html ){
   const template = document.createElement( 'template' );
   html = html.trim();
