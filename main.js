77df20d775aa5d487a0ef07ba00657baf903ab55
Gpossas <gpossas@protonmail.com>
2023-09-19 15:17:57 -0300

    selecting vertices in board

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ const board = document.querySelector( '#board' );
 
 setBoardDimensions( board, vertexSize, rows, columns )
 createBoard( rows, columns );
+
+function hovering(e){
+  console.log(e.target)
+}
+
+board.addEventListener( 'mousedown', ( e ) => {
+  console.log(e.target);
+  board.addEventListener( 'mouseover', hovering );
+});
+
+board.addEventListener('mouseup', (e) => {
+  board.removeEventListener('mouseover', hovering);
+})
