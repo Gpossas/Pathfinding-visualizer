ba87c5f202799fede860c34cff38f55501653ea2
Gpossas <gpossas@protonmail.com>
2023-09-20 19:52:39 -0300

    add selecting vertex when pressing mouse

--- src/App.svelte
+++ src/App.svelte
@@ -4,17 +4,33 @@
   import { vertexSize } from "./helpers/vertex.js"
 
   const graph = createBoard( rows, columns );
+
+  let isPressing = false;
+  /**
+   * @param { Event } e
+   */
+  function addOrRemoveWall(e){
+    console.log('pressing...')
+    isPressing = true;
+  }
+  
+  function stopSelecting(e){
+    console.log('stop pressing');
+    isPressing = false;
+  }
 </script>
 
 <div class="container">
   <div 
+    on:mousedown|preventDefault={addOrRemoveWall} on:mouseup={stopSelecting}
     id="board" 
     style="grid-template-rows: repeat({ rows }, { vertexSize }px); 
-      grid-template-columns: repeat({ columns }, { vertexSize }px);" 
-    >
+    grid-template-columns: repeat({ columns }, { vertexSize }px);" 
+    role="grid" tabindex="-1" aria-label="Board"
+      >
     {#each graph as line, row }
       {#each line as vertex, column }
-        <VertexComponent { row } {column} />
+        <VertexComponent {isPressing} { row } {column} />
       {/each}
     {/each}
   </div>
--- src/lib/Vertex.svelte
+++ src/lib/Vertex.svelte
@@ -17,10 +17,21 @@
 
   export let row;
   export let column;
+  export let isPressing;
 
+  /**
+   * @param { Event } e
+   */
+   function selectItems(e){
+    console.log(e.target);
+  }
 </script>
 
-<div class="vertex" id="row:{row} column:{column}"></div>
+{#if isPressing}
+  <div class="vertex" on:mouseenter|once={selectItems} id="row:{row} column:{column}" role="cell" tabindex="-1" aria-label="vertex"></div>
+{:else}
+  <div class="vertex" id="row:{row} column:{column}"></div>
+{/if}
 
 <style>
   .vertex{
