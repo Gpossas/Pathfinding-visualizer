6d1c7e94e06878e8122dd41234f5e0de091ee965
Gpossas <gpossas@protonmail.com>
2023-09-20 20:54:04 -0300

    fix identation

--- src/App.svelte
+++ src/App.svelte
@@ -5,34 +5,33 @@
 
   const graph = createBoard( rows, columns );
 
-  let isPressing = false;
-  /**
-   * @param { Event } e
-   */
-  function startSelecting(e){
+  /** @param { Event } e */
+  function startSelecting( e ){
     console.log('pressing...')
     isPressing = true;
   }
-  
-  function stopSelecting(e){
+  /** @param { Event } e */
+  function stopSelecting( e ){
     console.log('stop pressing');
     isPressing = false;
   }
+
+  let isPressing = false;
 </script>
 
 <div class="container">
   <div 
-    on:mousedown|preventDefault={startSelecting} on:mouseup={stopSelecting}
+    on:mousedown|preventDefault={ startSelecting } on:mouseup={ stopSelecting }
     id="board" 
     style="grid-template-rows: repeat({ rows }, { vertexSize }px); 
     grid-template-columns: repeat({ columns }, { vertexSize }px);" 
     role="grid" tabindex="-1" aria-label="Board"
       >
-    {#each graph as line, row }
-      {#each line as vertex, column }
-        <VertexComponent {isPressing} { row } {column} />
-      {/each}
-    {/each}
+    { #each graph as line, row }
+      { #each line as vertex, column }
+        <VertexComponent { isPressing } { row } {column} />
+      { /each }
+    { /each }
   </div>
 </div>
 
