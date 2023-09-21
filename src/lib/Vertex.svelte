4610a580e29341907f2a7d10c79715a1f4a1a0b7
Gpossas <gpossas@protonmail.com>
2023-09-20 21:20:54 -0300

    remove unused class

--- src/lib/Vertex.svelte
+++ src/lib/Vertex.svelte
@@ -1,20 +1,4 @@
 <script>
-  class Vertex {
-    /**
-     * @param { Array } coordinates [x, y]
-     */
-    constructor( coordinates ){
-      this.coordinates = coordinates;
-      this.isWall = false;
-      this.start = false;
-      this.target = false;
-      this.visited = false;
-      this.explored = false;
-      this.isShortestPath = false;
-      this.previous = undefined;
-    }
-  }
-
   export let row;
   export let column;
   export let isPressing;
