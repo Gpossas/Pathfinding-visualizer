72bb4c9714e2777e3b64087d5ded603db0fb121f
Gpossas <gpossas@protonmail.com>
2023-09-25 10:10:59 -0300

    create queue datastructure

---
+++ src/lib/helpers/queue.js
@@ -0,0 +1,31 @@
+class NodeStructure{
+  constructor( value, previous = null){
+    this.value = value;
+    this.previous = previous;
+  }
+}
+
+class Queue{
+  constructor(){
+    this.front = null;
+    this.rear = null;
+  }
+
+  enqueue( value ) {
+    this.isEmpty() ?
+    this.front = this.rear = new NodeStructure( value ) :
+    this.rear = this.rear.previous = new NodeStructure( value );
+  }
+
+  dequeue(){
+    if ( this.isEmpty() ) return;
+
+    const value = this.front.value;
+    this.front = this.front.previous;
+    return value;
+  }
+
+  isEmpty(){
+    return ( this.front === null );
+  }
+}
