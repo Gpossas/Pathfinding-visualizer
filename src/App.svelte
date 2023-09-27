0c2e4e262053f49574ddca56122a9de976db7bee
Gpossas <gpossas@protonmail.com>
2023-09-26 21:19:29 -0300

    use store graph

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,20 @@
 <script>
   import Board from './lib/Board.svelte';
-  import { createBoard, rows, columns } from './lib/helpers/board';
+  import { rows, columns } from './lib/helpers/board';
   import { dfs } from './lib/pathfinding algorithms.js';
+  import { graph } from './lib/helpers/store.js';
 
-  const graph = createBoard( rows, columns );  
-  let startVertex = graph[Math.trunc( rows / 2 )][1];
-  let targetVertex = graph[Math.trunc( rows / 2 )][columns - 2];
+  let startVertex = $graph[Math.trunc( rows / 2 )][1];
+  let targetVertex = $graph[Math.trunc( rows / 2 )][columns - 2];
   startVertex.isStart = true;
   targetVertex.isTarget = true;
 </script>
 
 <header>
-  <button on:click={ () => dfs( startVertex, graph ) } type="submit">DFS</button>
+  <button on:click={ () => dfs( startVertex ) } type="submit">DFS</button>
 </header>
 
 <Board 
-  { graph } 
   { startVertex } 
   { targetVertex } 
   { rows }
