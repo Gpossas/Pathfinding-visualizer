8964dd24b3c14f28b88e4c9f73d5064ebc596f92
Gpossas <gpossas@protonmail.com>
2023-09-29 16:23:45 -0300

    add BFS

--- src/lib/pathfinding algorithms.js
+++ src/lib/pathfinding algorithms.js
@@ -43,3 +43,57 @@ export async function dfs( start ){
   await explore( ...start.coordinates );
   traversePathTraveled();
 }
+
+/** @param { Vertex } start start vertex */
+export async function bfs( start ){
+  const queue = new Queue();
+  queue.enqueue( start );
+
+  while ( !queue.isEmpty() ){
+    let vertex = queue.dequeue();
+    
+    if ( vertex.isTarget ){
+      return buildShortestPath( vertex );
+    }
+
+    const [row, column] = vertex.coordinates;
+    const left = [row, column - 1];
+    const right = [row, column + 1];
+    const up = [row - 1, column];
+    const down = [row + 1, column];
+    explore( ...left, vertex );
+    explore( ...right, vertex );
+    explore( ...up, vertex );
+    explore( ...down, vertex );
+
+    get(graph)[row][column].visited = true;
+    await sleep( 10 );
+  }
+
+  function explore( row, column, vertex ){
+    if ( 
+      isOutOfBounds( row, column ) 
+      || get(graph)[row][column].isWall 
+      || get(graph)[row][column].visited 
+    ) return;
+    
+    get(graph)[row][column].explored = true;
+    queue.enqueue( get(graph)[row][column] );
+    get(graph)[row][column].previous = vertex;
+  }
+
+  async function buildShortestPath( vertex ){
+    const pathStack = [];
+    while ( vertex ){
+      pathStack.push( vertex );
+      vertex = vertex.previous;
+    }
+
+    while ( pathStack.length > 0 ){
+      vertex = pathStack.pop();
+      const [row, column] = vertex.coordinates;
+      get(graph)[row][column].previous = true;
+      await sleep( 10 );
+    }
+  }
+}
