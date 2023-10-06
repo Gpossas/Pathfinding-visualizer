8c3314e50c94c68a9f1ac449ab5401bc45a3931e
Gpossas <gpossas@protonmail.com>
2023-10-06 17:35:25 -0300

    add a* algorithm

--- src/lib/pathfinding algorithms.js
+++ src/lib/pathfinding algorithms.js
@@ -94,7 +94,7 @@ export async function dijkstra( start ){
 
   while ( priorityQueue.length > 0 ){
     const vertex = heapPop( priorityQueue );
-    debugger
+    
     if ( vertex.isTarget ){
       return buildShortestPath( vertex );
     }
@@ -138,18 +138,60 @@ export async function dijkstra( start ){
  * @param { Vertex } start 
  * @param { Vertex } target 
  */
-async function aStar( start, target ){
-  start.h = getHeuristic( ...start.coordinates, ...target.coordinates );
-  start.f = start.h;
+export async function aStar( start, target ){
+  start.g = 0;
+  start.f = getHeuristic( ...start.coordinates, ...target.coordinates );
 
   // when comparing the priority, if items have the same f value, it will choose the one with the minimum g value
   // I choose g value instead of h value because the heuristic cannot handle the weights in the path
-  const priorityQueue = [ [ start.f, start.g, start ] ];
+  const priorityQueue = [ [ start.f, start.f, start ] ];
   while ( priorityQueue.length > 0 ){
+    const vertex = heapPop( priorityQueue );
     
+    if ( vertex.isTarget ){
+      return buildShortestPath( vertex );
+    }
+    if ( vertex.visited ){
+      continue;
+    }
+
+    graph.compute( ...vertex.coordinates, 'visited' );
+
+    const [row, column] = vertex.coordinates;
+    const left = [row, column - 1];
+    const right = [row, column + 1];
+    const up = [row - 1, column];
+    const down = [row + 1, column];
+    explore( ...left, vertex, target, priorityQueue );
+    explore( ...up, vertex, target, priorityQueue );
+    explore( ...right, vertex, target, priorityQueue );
+    explore( ...down, vertex, target, priorityQueue );
+    await sleep( 10 );
   }
 
-  /** {*} @returns manhattan distance */
+  function explore( row, column, vertex, target, priorityQueue ){
+    if (
+      isOutOfBounds( row, column )
+      || get(graph)[row][column].isWall 
+      || get(graph)[row][column].visited
+    ) return;
+
+    graph.compute( row, column, 'explored' );
+    const neighbor = get(graph)[row][column];
+    const g_cost = neighbor.value + vertex.g;
+    const h_cost = getHeuristic( ...neighbor.coordinates, ...target.coordinates );
+    const f_cost = g_cost + h_cost;
+
+    if ( neighbor.f === undefined || f_cost < neighbor.f ){
+      neighbor.f = f_cost;
+      neighbor.g = g_cost;
+
+      heapPush( priorityQueue, [ f_cost, h_cost, neighbor ] );
+      neighbor.previous = vertex;
+    }
+  }
+
+  /** @returns manhattan distance */
   function getHeuristic( x1, y1, x2, y2 ){
     return Math.abs( x1 - x2 ) + Math.abs( y1 - y2 );
   }
