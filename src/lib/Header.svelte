f0de06940cf1f39b8b9369cafe63a772c4dd9d93
Gpossas <gpossas@protonmail.com>
2023-10-09 18:32:20 -0300

    create header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { dfs, bfs, dijkstra, aStar } from './pathfinding algorithms.js';
+
+  export let startVertex;
+  export let targetVertex;
+</script>
+
+<header>
+  <button on:click={ () => dfs( startVertex ) } type="submit">DFS</button>
+  <button on:click={ () => bfs( startVertex ) } type="submit">BFS</button>
+  <button on:click={ () => dijkstra( startVertex ) } type="submit">Dijkstra</button>
+  <button on:click={ () => aStar( startVertex, targetVertex ) } type="submit">A*</button>
+</header>
