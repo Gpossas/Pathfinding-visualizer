aa9d5aa005d7911de57be03a9498daaad5761e34
Gpossas <gpossas@protonmail.com>
2023-10-28 14:29:50 -0300

    add start legend

---
+++ src/lib/Legends.svelte
@@ -0,0 +1,16 @@
+<div class="legends">
+  <ul>
+    <li>
+      <img src="../src/assets/person.svg" alt="person, start vertex" width="35px" height="35px">
+      <span>start</span>
+    </li>
+  </ul>
+</div>
+
+<style>
+  li{
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+</style>
