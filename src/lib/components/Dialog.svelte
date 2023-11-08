000399616885bf0dbb2db41eb88afd6fd271a5c0
Gpossas <gpossas@protonmail.com>
2023-11-08 20:30:56 -0300

    add: key to dialog

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -11,12 +11,12 @@
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <dialog bind:this={ dialog } on:click={ () => dialog.close() }>
   <ul>
-    <li>click or hold <span>left</span> click mouse to <span>add walls</span>.</li>
-    <li>click or hold <span>right</span> click mouse to <span>remove walls</span>.</li>
+    <li>click or hold <span>left</span> mouse button to <span>add walls</span>.</li>
     <li>Hold <span>shift</span> to <span>add weights</span>.</li>
-    <li>click or hold <span>right</span> click mouse to <span>remove weights</span>.</li>
+    <li>click or hold <span>right</span> mouse button to <span>remove walls</span> or weights.</li>
     <li>move <span>person</span> and <span>target</span> anywhere on the board.</li>
-    <li>If you <span>move</span> person or target after you running an algorithm it will <span>rebuild</span> the path immediately, so you can see other paths <span>BLAZINGLY FAST</span></li>
+    <li>when <span>key</span> is added the algorithm will need to find it first then the target.</li>
+    <li>If you <span>move</span> person or target after you running an algorithm it will <span>rebuild</span> the path immediately, so you can see other paths <span>BLAZINGLY FAST</span>!</li>
   </ul>
 </dialog>
 
